66296b740112725a93b27dd2a20615fceb5774c4
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-16 13:35:04 +0300

    context in record programming

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 /* Your Code Here */
+// function to create a single employee record
+function createEmployeeRecord(employeeArray) {
+    return {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
 
+// Function to create multiple employee records from an array of arrays
+function createEmployeeRecords(employeeArrays) {
+    return employeeArrays.map(createEmployeeRecord);
+}
+
+// Function to create a timeIn event
+function createTimeInEvent(dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    this.timeInEvents.push({type: "TimeIn", date, hour: parseInt(hour, 10)});
+    return this;
+}
+
+// Function to create a timeOut event
+function createTimeOutEvent(dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    this.timeOutEvents.push({type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date).hour;
+    let timeOut = this.timeOutEvents.find(e => e.date === date).hour;
+    return (timeOut - timeIn) / 100;    
+}
+    
+function wagesEarnedOnDate(date) {
+    let hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => employee.firstName === firstNameString);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, record) => total + allWagesFor.call(record), 0);
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
